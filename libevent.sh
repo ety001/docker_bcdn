404175e26f57f766a2d10d68476c5f49514fbc9c
ety001 <ety001@domyself.me>
2018-02-04 06:16:26 +0800

    rm shell

--- libevent.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-unzip Libevent-release-2.1.8-stable.zip && \
-cd Libevent-release-2.1.8-stable && \
-./autogen.sh && \
-./configure --prefix=/usr && \
-make && make install && \
-ls -al /usr/lib | grep libevent && \
-cd .. && rm -rf Libevent-release-2.1.8-stable.zip Libevent-release-2.1.8-stable
--- protobuf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-unzip protobuf-master.zip && \
-cd protobuf-master && \
-./autogen.sh && \
-./configure && \
-make && make check && make install && \
-ldconfig && \
-protoc --version && \
-cd .. && rm -rf /data/protobuf-master.zip /data/protobuf-master
